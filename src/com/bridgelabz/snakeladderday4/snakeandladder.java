74dfab9de9c2c76b9e4dc28ab448f987553df67c
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 21:24:41 +0530

    UC 3 "NO PLAY SNAKE & LADDER"

--- src/com/bridgelabz/snakeladderday4/snakeandladder.java
+++ src/com/bridgelabz/snakeladderday4/snakeandladder.java
@@ -2,11 +2,37 @@ package com.bridgelabz.snakeladderday4;
 import java.util.Random;
 public class snakeandladder {
 
-    public static void main(String[] args) {
-        int start = 0;
-        System.out.println("Starting position of player is: " + start);
-        Random random = new Random();
-        int roll = random.nextInt(7);
-        System.out.println(roll);
-    }
+        public static final int NO_PLAY = 0;
+        public static final int LADDER = 1;
+        public static final int SNAKE = 2;
+        public static final int START = 0;
+        public static void main (String[] args){
+            int pos = 0;
+            System.out.println("Starting position of player is: " + START);
+            Random random = new Random();
+            int roll = random.nextInt(7);
+            System.out.println("Number on rolled dice is : " + roll);
+            int con = random.nextInt(3);
+            System.out.println("Condition: " + con);
+            switch (con) {
+                case NO_PLAY:
+                    System.out.println("New Position is: " + pos);
+                    break;
+                case LADDER:
+                    pos = pos + roll;
+                    System.out.println("New Position is: " + pos);
+                    break;
+                case SNAKE:
+                    pos = pos - roll;
+                    if (pos >= 0)
+                        System.out.println("New Position is: " + pos);
+                    else
+                        pos = 0;
+                    System.out.println("New Position is: " + pos);
+                    break;
+                default:
+                    System.out.println("Default");
+                    break;
+            }
+        }
     }
