fa6db2e5a2222a74fa68180816cdddbc3e0aaf1e
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 21:18:24 +0530

    UC 2 "NUMBER B/W 1 TO 6"

--- src/com/bridgelabz/snakeladderday4/snakeandladder.java
+++ src/com/bridgelabz/snakeladderday4/snakeandladder.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.snakeladderday4;
-
+import java.util.Random;
 public class snakeandladder {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int start = 0;
         System.out.println("Starting position of player is: " + start);
-}
+        Random random = new Random();
+        int roll = random.nextInt(7);
+        System.out.println(roll);
+    }
+    }
