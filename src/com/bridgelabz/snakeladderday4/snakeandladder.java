1ebff2a0ab7a06145fc5b64f7037182227ec422a
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 21:30:39 +0530

    UC 6 "NUMBER OF DICE TO WON THE GAME EXACT POSITION"

--- src/com/bridgelabz/snakeladderday4/snakeandladder.java
+++ src/com/bridgelabz/snakeladderday4/snakeandladder.java
@@ -1,27 +1,25 @@
 package com.bridgelabz.snakeladderday4;
 import java.util.Random;
 public class snakeandladder {
-
     public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
     public static final int START = 0;
-
-
     public static void main(String[] args){
-        int pos = 0;
+        int pos = 0, cnt1 = 0, cnt2 = 0, cnt3 = 0, cnt4 = 0;
         System.out.println("Starting position of player is: " + START);
         while(pos < 100){
             Random random = new Random();
             int roll = random.nextInt(7);
+            cnt1++;
             System.out.println("Number on rolled dice is : " +roll);
             Random obj = new Random();
             int con = obj.nextInt(4);
-
             System.out.println("Condition: " +con);
             switch (con){
                 case NO_PLAY:
                     System.out.println("New Position is: " + pos);
+                    cnt4++;
                     break;
                 case LADDER:
                     pos = pos + roll;
@@ -31,11 +29,14 @@ public class snakeandladder {
                         pos = pos - roll;
                         System.out.println("Remains at same position: " + pos);
                     }
+                    cnt2++;
                     break;
                 case SNAKE:
                     pos = pos - roll;
-                    if (pos <= 100){
-                        if (pos >= 0 ){
+                    if (pos <= 100)
+                    {
+                        if (pos >= 0)
+                        {
                             System.out.println("New Position is: " + pos);
                         }
                         else{
@@ -43,9 +44,10 @@ public class snakeandladder {
                             System.out.println("New Position is: " + pos);
                         }
                     }
-                    else{
+                    else {
                         System.out.println("New position is:" + pos);
                     }
+                    cnt3++;
                     break;
                 default:
                     System.out.println("Default");
@@ -53,10 +55,12 @@ public class snakeandladder {
             }
         }
         System.out.println();
+        System.out.println("Player ROLLED DICE "+cnt1+" time and got SNAKE "+cnt3+" times and got LADDER "+cnt2+" times and got NO PLAY "+cnt4+" times to win the game");
         if (pos == 100)
             System.out.println("-------------------");
         System.out.println("    PLAYER WON THIS GAME...!!");
         System.out.println("-------------------");
     }
     }
+
 
