42cf444571d8e15c82bb20531dca1cb1fd862fdf
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 21:28:45 +0530

    UC 5 "PLAYER WON THE GAME EXACT POSITION 100"

--- src/com/bridgelabz/snakeladderday4/snakeandladder.java
+++ src/com/bridgelabz/snakeladderday4/snakeandladder.java
@@ -7,33 +7,45 @@ public class snakeandladder {
     public static final int SNAKE = 2;
     public static final int START = 0;
 
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args){
         int pos = 0;
         System.out.println("Starting position of player is: " + START);
-        while (pos <= 100)
-        {
+        while(pos < 100){
             Random random = new Random();
             int roll = random.nextInt(7);
-            System.out.println("Number on rolled dice is : " + roll);
-            int con = random.nextInt(5);
-            System.out.println("Condition: " + con);
-            switch (con)
-            {
+            System.out.println("Number on rolled dice is : " +roll);
+            Random obj = new Random();
+            int con = obj.nextInt(4);
+
+            System.out.println("Condition: " +con);
+            switch (con){
                 case NO_PLAY:
                     System.out.println("New Position is: " + pos);
                     break;
                 case LADDER:
                     pos = pos + roll;
-                    System.out.println("New Position is: " + pos);
+                    if(pos <= 100)
+                        System.out.println("New Position is: " + pos);
+                    else{
+                        pos = pos - roll;
+                        System.out.println("Remains at same position: " + pos);
+                    }
                     break;
                 case SNAKE:
                     pos = pos - roll;
-                    if (pos >= 0)
-                        System.out.println("New Position is: " + pos);
-                    else
-                        pos = 0;
-                    System.out.println("New Position is: " + pos);
+                    if (pos <= 100){
+                        if (pos >= 0 ){
+                            System.out.println("New Position is: " + pos);
+                        }
+                        else{
+                            pos = 0;
+                            System.out.println("New Position is: " + pos);
+                        }
+                    }
+                    else{
+                        System.out.println("New position is:" + pos);
+                    }
                     break;
                 default:
                     System.out.println("Default");
@@ -41,9 +53,10 @@ public class snakeandladder {
             }
         }
         System.out.println();
-        if (pos >= 100)
+        if (pos == 100)
             System.out.println("-------------------");
-        System.out.println(" PLAYER WON THE GAME");
+        System.out.println("    PLAYER WON THIS GAME...!!");
         System.out.println("-------------------");
     }
     }
+
