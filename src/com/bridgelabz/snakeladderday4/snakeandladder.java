bb0f12422eb7fe74b11086acbc7ddb99464d649d
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 21:26:46 +0530

    UC 4 "LAYER WON THE GAME"

--- src/com/bridgelabz/snakeladderday4/snakeandladder.java
+++ src/com/bridgelabz/snakeladderday4/snakeandladder.java
@@ -2,19 +2,24 @@ package com.bridgelabz.snakeladderday4;
 import java.util.Random;
 public class snakeandladder {
 
-        public static final int NO_PLAY = 0;
-        public static final int LADDER = 1;
-        public static final int SNAKE = 2;
-        public static final int START = 0;
-        public static void main (String[] args){
-            int pos = 0;
-            System.out.println("Starting position of player is: " + START);
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static final int START = 0;
+
+    public static void main(String[] args)
+    {
+        int pos = 0;
+        System.out.println("Starting position of player is: " + START);
+        while (pos <= 100)
+        {
             Random random = new Random();
             int roll = random.nextInt(7);
             System.out.println("Number on rolled dice is : " + roll);
-            int con = random.nextInt(3);
+            int con = random.nextInt(5);
             System.out.println("Condition: " + con);
-            switch (con) {
+            switch (con)
+            {
                 case NO_PLAY:
                     System.out.println("New Position is: " + pos);
                     break;
@@ -35,4 +40,10 @@ public class snakeandladder {
                     break;
             }
         }
+        System.out.println();
+        if (pos >= 100)
+            System.out.println("-------------------");
+        System.out.println(" PLAYER WON THE GAME");
+        System.out.println("-------------------");
+    }
     }
